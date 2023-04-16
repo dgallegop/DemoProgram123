a89f094babb702f677183dc222957d7df67c2a35
diego.gallego.at <dgallegop@hotmail.com>
2023-04-15 21:47:16 -0500

    prueba

---
+++ src/main/java/com/example/DemoProgram123/DemoProgram123Application.java
@@ -0,0 +1,13 @@
+package com.example.DemoProgram123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProgram123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProgram123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DemoProgram123/DemoProgram123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoProgram123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProgram123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
